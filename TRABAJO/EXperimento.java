4652605e696a3af7cb951ebc184f257dcc5d906f
Andres-15Sanchez <andrettygabo5543@gmail.com>
2024-10-21 17:11:13 -0600

    java

---
+++ TRABAJO/EXperimento.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Andres
+ */
+public class QuienesmayorABC {
+    public static void main(String[] args) {
+        byte A = Byte.parseByte(JOptionPane.showInputDialog(null,"Ingrese un numero"));
+        byte B = Byte.parseByte(JOptionPane.showInputDialog(null,"Ingresa un numero"));
+        byte C = Byte.parseByte(JOptionPane.showInputDialog(null,"Ingresa una numero"));
+        if (A==B || A==C || C ==B){
+        System.out.print("A,B,C son iguales");
+    }
+        if(A>B || A>C){
+            System.out.print(A +" Es mayor");
+        }
+        if (B>A && B>C){
+            System.out.print(B + " Es mayor");
+        }
+        if (A<C && B< C){
+            System.out.print(C + " Es mayor");
+        }
+    }
+}
