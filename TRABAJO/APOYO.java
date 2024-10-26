285d5a9d4fd25feed9fc01b8bfc7eef2704ce4e0
Andres-15Sanchez <andrettygabo5543@gmail.com>
2024-10-25 19:19:05 -0600

    ttt

--- TRABAJO/APOYO.java
+++ TRABAJO/APOYO.java
@@ -1,39 +1,48 @@
-public class ApoyoEstudiantesSinStrings {
+
+import javax.swing.JOptionPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Andres
+ */
+public class APOYO {
     public static void main(String[] args) {
-        // Monto total del apoyo
-        double total = 189000;
-
-        // Identificadores de los talleres (números para evitar Strings)
-        int[] talleres = {
-            1, // Básquet
-            2, // Voleibol
-            3, // Artes
-            4, // Fútbol
-            5, // Taekwondo
-            6, // Ajedrez
-            7, // Música
-            8  // Atletismo
-        };
-
-        // Montos asignados a cada taller
-        double[] montos = {
-            9000,   // Básquet
-            15000,  // Voleibol
-            50000,  // Artes
-            15000,  // Fútbol
-            50000,  // Taekwondo
-            25000,  // Ajedrez
-            12000,  // Música
-            13000   // Atletismo
-        };
-
-        // Imprimir el identificador del taller, monto asignado y porcentaje
-        System.out.printf("%-10s %-10s %-10s%n", "Taller", "Monto", "Porcentaje");
-        System.out.println("------------------------------------");
-
-        for (int i = 0; i < talleres.length; i++) {
-            double porcentaje = (montos[i] / total) * 100;
-            System.out.printf("%-10d %-10.2f %-10.2f%%%n", talleres[i], montos[i], porcentaje);
-        }
-    }
+        JOptionPane.showMessageDialog(null,""" 
+        EL TESJI RECIBIO UN APOYO DEL GOBIERNO DE 189 MIL PESOS
+             PARA LAS ACTIVIDADES CULTURALES Y DEPORTIVAS
+                                           """);
+        int APOYO_T =189000;
+        int PARTE_B=9000;
+        int PARTE_V=15000;
+        int PARTE_AR=9000;
+        int PARTE_F=15000;
+        int PARTE_T=9000;
+        int PARTE_AJ=15000;
+        int PARTE_M=9000;
+        int PARTE_ATL=15000;
+        double porcen=0.0;
+        
+            porcen =(Double)((PARTE_B*100.0)/APOYO_T);
+             System.out.println("Basquetball   $9000.00 %1f");
+            porcen = (Double)((PARTE_V*100.0)/APOYO_T);
+             System.out.println("Voleibol   $15000.00  "+ porcen+"%");
+            porcen =(Double)((PARTE_AR*100.0)/APOYO_T);
+             System.out.println("Artes   $50000.00  "+ porcen+"%");
+            porcen = (Double)((PARTE_F*100.0)/APOYO_T);
+             System.out.println("Futbol  $25000.00  "+ porcen+"%");
+            porcen =(Double)((PARTE_T*100.0)/APOYO_T);
+             System.out.println("Taekoandow  $50000.00  "+ porcen+"%");
+            porcen = (Double)((PARTE_AJ*100.0)/APOYO_T);
+             System.out.println("Ajedres   $25000.00  "+ porcen+"%");
+            porcen =(Double)((PARTE_M*100.0)/APOYO_T);
+             System.out.println("Musica  $12000.00  "+ porcen+"%");
+            porcen = (Double)((PARTE_ATL*100.0)/APOYO_T);
+             System.out.println("Atletismo   $13000.00  "+ porcen+"%");
+             
+    }
 }
